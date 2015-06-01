a3291593eee6370e41d61b7acabad5d5348b9256
DAloG <dalog@me.com>
2015-06-01 14:22:17 +0300

    Add tests for ticket parsing

--- iOS/UZTickets/src/core/UZTTicketInfo.m
+++ iOS/UZTickets/src/core/UZTTicketInfo.m
@@ -10,4 +10,76 @@
 
 @implementation UZTTicketInfo
 
++ (instancetype)newWithString:(NSString *)infoString
+{
+    return [[self alloc] initWithString:infoString];
+}
+
+- (instancetype)initWithString:(NSString *)infoString
+{
+    self = [super init];
+    if (self == nil) return nil;
+    
+    _infoString = infoString;
+    
+    NSCharacterSet* newlines = [NSCharacterSet newlineCharacterSet];
+    
+    NSScanner* scanner = [NSScanner scannerWithString:infoString];
+    scanner.charactersToBeSkipped = newlines;
+    
+    NSString* trainName;
+    [scanner scanUpToCharactersFromSet:newlines intoString:&trainName];
+    
+    NSString* departureStation;
+    [scanner scanUpToCharactersFromSet:newlines intoString:&departureStation];
+    
+    NSString* arrivalStattion;
+    [scanner scanUpToCharactersFromSet:newlines intoString:&arrivalStattion];
+    
+    NSString* departureDate;
+    [scanner scanUpToCharactersFromSet:newlines intoString:&departureDate];
+    
+    NSString* arrivalDate;
+    [scanner scanUpToCharactersFromSet:newlines intoString:&arrivalDate];
+    
+    NSInteger wagonNumber;
+    [scanner scanInteger:&wagonNumber];
+    
+    NSString* wagonKind;
+    scanner.scanLocation += 1;
+    [scanner scanUpToCharactersFromSet:newlines intoString:&wagonKind];
+    
+    NSInteger seatNumber;
+    [scanner scanInteger:&seatNumber];
+    
+    NSString* seatKind;
+    scanner.scanLocation += 1;
+    [scanner scanUpToCharactersFromSet:newlines intoString:&seatKind];
+    
+    [scanner scanUpToCharactersFromSet:newlines intoString:NULL];
+    
+    NSString* passenger;
+    [scanner scanUpToCharactersFromSet:newlines intoString:&passenger];
+   
+    [scanner scanUpToCharactersFromSet:newlines intoString:NULL];
+    [scanner scanUpToCharactersFromSet:newlines intoString:NULL];
+    
+    NSString* ticketCode;
+    [scanner scanUpToCharactersFromSet:newlines intoString:&ticketCode];
+    
+    _trainName = trainName;
+    _departureStation = departureStation;
+    _arrivalStation = arrivalStattion;
+    _departureTime = nil;
+    _arrivalTime = nil;
+    _wagonNumber = @(wagonNumber);
+    _wagonType = wagonKind;
+    _seatNumber = @(seatNumber);
+    _seatType = seatKind;
+    _passengerFullname = passenger;
+    _ticketCode = ticketCode;
+    
+    return self;
+}
+
 @end
